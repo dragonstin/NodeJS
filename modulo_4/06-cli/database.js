f4d439f8fa3819cfdd0e481555fa6ddff0597234
fernandadagostin <nanda.dagostin@gmail.com>
2019-11-30 12:23:25 -0500

    concluído modulo 04

--- modulo_4/06-cli/database.js
+++ modulo_4/06-cli/database.js
@@ -1,5 +1,6 @@
 const {
-    readFile
+    readFile,
+    writeFile
 } = require('fs')
 
 const {
@@ -7,6 +8,7 @@ const {
 } = require('util')
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 // outra forma de obter dados do json
 //const dadosJson = require('./herois.json')
@@ -21,14 +23,104 @@ class Database {
         return JSON.parse(arquivo.toString())
     }
     
-    escreverArquivo() {
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
+    }
 
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <=2 ? heroi.id : Date.now();
+        /**
+         * {
+         *  nome: Flash,
+         *  poder: Velocidade
+         * }
+         * 
+         * {
+         *  id: 123871327
+         * }
+         * 
+         * resultado **
+         * {
+         *  nome: Flash,
+         *  poder: Velocidade
+         *  id: 1
+         * }
+         */
+        const heroiComId = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ]
+
+        /**
+         * [
+         *  {
+         *    nome: Flash
+         *  }
+         * ]
+         * 
+         * {
+         *  nome: Batman
+         * }
+         * 
+         * resultado **
+         * [
+         *  {
+         *   nome: Flash
+         *  },
+         *  {
+         *   nome: Batman
+         *  }
+         * ]
+         */
+        const resultado = await this.escreverArquivo(dadosFinal)
+        return resultado;
     }
 
     async listar(id) {
         const dados = await this.obterDadosArquivo()
         const dadosFiltrados = dados.filter(item =>(id ? (item.id === id) : true))
         return dadosFiltrados
+    }
+
+    async remover(id) {
+        if(!id){
+            return await this.escreverArquivo([])
+        }
+
+        const dados = await this.obterDadosArquivo()
+        console.log('id ',id)
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice=== -1){
+            throw Error('O id informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
+
+    async atualizar(id, modificacoes) {
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice === -1){
+            throw Error('O herou informado não existe.')
+        }
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados,
+            objetoAtualizar
+        ])
     }
 }
 
